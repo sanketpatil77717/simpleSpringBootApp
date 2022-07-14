f480a1b4fe77a68a6613fd3b898c22a17ed1026d
sanket <sp.development77717@gmail.com>
2022-07-14 12:57:03 +0530

    Adding Basic Spring Boot Application

---
+++ src/main/java/com/ccf/DemoProjectForCicdApplication.java
@@ -0,0 +1,13 @@
+package com.ccf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForCicdApplication.class, args);
+	}
+
+}
