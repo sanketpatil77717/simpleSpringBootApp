6131c6718d359d8270e6cc15ab716a526cccec6d
sanket <sp.development77717@gmail.com>
2022-07-14 17:13:28 +0530

    Added new handler mapping for /employee path to get Json response

--- src/main/java/com/ccf/controller/HomeController.java
+++ src/main/java/com/ccf/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.ccf.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ccf.dto.Employee;
+
 @RestController
 public class HomeController {
-	
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "Welcome to HomePage ! This Project is deployed using Jenkins and CI/CD";
+	}
+
+	@GetMapping("/employee")
+	public ResponseEntity<Employee> getEmployee() {
+
+		return new ResponseEntity<Employee>(new Employee("Tony", "Stark", "tony@stark.com"), HttpStatus.OK);
+
 	}
 
 }
