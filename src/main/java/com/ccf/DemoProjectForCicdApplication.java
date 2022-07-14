6bfc34047b8cb03271f9c34777c45302d33d6cc5
sanket <sp.development77717@gmail.com>
2022-07-14 15:47:12 +0530

    Added Changes Necessary to convert packaging from jar to war in Pom.xml.Changed scope of tomcat server dependency to provided.Added changes in DemoProjectForCicdApplication.java

--- src/main/java/com/ccf/DemoProjectForCicdApplication.java
+++ src/main/java/com/ccf/DemoProjectForCicdApplication.java
@@ -2,10 +2,19 @@ package com.ccf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoProjectForCicdApplication {
+public class DemoProjectForCicdApplication extends SpringBootServletInitializer  {
 
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(DemoProjectForCicdApplication.class);
+	    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectForCicdApplication.class, args);
 	}
