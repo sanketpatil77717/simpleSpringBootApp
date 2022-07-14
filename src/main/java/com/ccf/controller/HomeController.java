99300dd14ac72333c6b29905e1851b956e3f8550
sanket <sp.development77717@gmail.com>
2022-07-14 16:53:39 +0530

    Added some reponse to root handler to check POLL SCM on Jenkins

--- src/main/java/com/ccf/controller/HomeController.java
+++ src/main/java/com/ccf/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "Welcome to HomePage !";
+		return "Welcome to HomePage ! This Project is deployed using Jenkins and CI/CD";
 	}
 
 }
